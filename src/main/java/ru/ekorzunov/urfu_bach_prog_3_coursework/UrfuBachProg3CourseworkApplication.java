c666450b54eab2198a805fe5b9cb4262d3ad385e
Evgeniy Korzunov <korzunov.e.v@gmail.com>
2024-09-21 16:45:14 +0500

    init empty project

---
+++ src/main/java/ru/ekorzunov/urfu_bach_prog_3_coursework/UrfuBachProg3CourseworkApplication.java
@@ -0,0 +1,13 @@
+package ru.ekorzunov.urfu_bach_prog_3_coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrfuBachProg3CourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrfuBachProg3CourseworkApplication.class, args);
+	}
+
+}
